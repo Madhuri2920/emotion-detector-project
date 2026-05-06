e956a84364eefe135c789668d20eb9340cfb3fe0
Madhuri2920 <146976108+Madhuri2920@users.noreply.github.com>
2026-05-07 00:25:28 +0530

    Update mywebscript.js

--- static/mywebscript.js
+++ static/mywebscript.js
@@ -1,1 +1,16 @@
 
+function RunSentimentAnalysis() {
+    let textToAnalyze = document.getElementById("textToAnalyze").value;
+    let responseBox = document.getElementById("system_response");
+
+    responseBox.innerHTML = "Analyzing...";
+
+    fetch("/emotionDetector?textToAnalyze=" + encodeURIComponent(textToAnalyze))
+        .then(response => response.text())
+        .then(data => {
+            responseBox.innerHTML = data;
+        })
+        .catch(error => {
+            responseBox.innerHTML = "Error: " + error;
+        });
+}
